5049ce5a13c0e4184f499ab0fd702f11f52ba09c
mako87 <marco.grassi@hotmail.it>
2016-02-24 09:18:44 +0100

    Nodejs-template

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,5 +1,5 @@
 module.exports = function(express){
-    var router = express['Router']();
+    var router = express["Router"]();
     router.get("/", function(req,res){
         res.sendStatus(200);
     });
