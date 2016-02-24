c296726ac8246680a08c1bb429b46e82799eed35
mako87 <marco.grassi@hotmail.it>
2016-02-24 09:34:55 +0100

    Nodejs-template

--- routes/errorHandler.js
+++ routes/errorHandler.js
@@ -1,8 +1,10 @@
 module.exports = function(logger) {
-    return function (err, req, res) {
+    return function (err, req, res, next) {
         if (err.name === "UnauthorizedError") {
             logger.info(req.ip+" "+err.message);
             res.status(401).send(err.message);
+        } else {
+            next();
         }
     };
 };
