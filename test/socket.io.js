1179f2a9b6154e0cbc0d9b082a212b96a11b57b6
mako87 <marco.grassi@hotmail.it>
2016-02-26 10:10:17 +0100

    Nodejs-template

--- test/socket.io.js
+++ test/socket.io.js
@@ -2,7 +2,33 @@
  * Created by Marco on 25/02/16.
  */
 var should = require("should");
+var io = require("socket.io-client");
+var config = require("../config/default.json");
 
-describe("Socket.io", function() {
+var socketURL = "http://0.0.0.0:"+config["webserver"]["port"];
 
+var options ={
+    transports: ["websocket"],
+    "force new connection": true
+};
+
+describe("Socket.io",function(){
+    describe("Message ember-data",function(){
+        it("Find",function(done){
+            var client = io.connect(socketURL,options);
+            client.on("connect",function(){
+                client.emit("ember-data",{cmd:"find",type:"user",params:"mgrassi"},done);
+            });
+            client.on("connect_error",done);
+        });
+
+        it("Create",function(done){
+            var client = io.connect(socketURL,options);
+            client.on("connect",function(){
+                client.emit("ember-data",{cmd:"create",type:"user",params:{id:"mgrassi2",password:"test"}},done);
+            });
+            client.on("connect_error",done);
+        });
+    });
 });
+
